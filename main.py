d5743c5ebacd7641ba1c7b34b5ef998392d94b32
Parth <ppancholi2@gmail.com>
2022-12-30 14:56:21 +0530

    My variable names
    Changed the variables for better readability

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-socketio = SocketIO(app)
+
+app = Flask( __name__)
+app.config[ 'SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+soc = SocketIO( app)
 
 
 @app.route('/')
@@ -11,15 +12,15 @@ def sessions():
     return render_template('session.html')
 
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
+def message_received( methods = [ 'GET', 'POST']):
+    print( 'Message received successfully!')
 
 
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
+@soc.on('my event')
+def handle_custom_event( json, methods = [ 'GET', 'POST']):
+    print('Received my event', str( json))
+    soc.emit( 'my response', json, callback = message_received)
+    
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    soc.run( app, debug = True)
